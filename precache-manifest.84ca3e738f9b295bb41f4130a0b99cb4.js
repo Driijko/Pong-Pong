96c154bf8562fa1ddcba3b473aba7dc69e389cab
Driijko <driijko@gmail.com>
2020-01-28 14:23:19 -0500

    Updates

---
+++ precache-manifest.84ca3e738f9b295bb41f4130a0b99cb4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc0ec89e15d7fd68aeec3508fe1ae3d",
+    "url": "/Pong-Pong/index.html"
+  },
+  {
+    "revision": "9f94a96afab7de4b95da",
+    "url": "/Pong-Pong/static/css/main.018325bb.chunk.css"
+  },
+  {
+    "revision": "9b0208c781ee0bbb71bb",
+    "url": "/Pong-Pong/static/js/2.4378ab9c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pong-Pong/static/js/2.4378ab9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f94a96afab7de4b95da",
+    "url": "/Pong-Pong/static/js/main.99d6a0a4.chunk.js"
+  },
+  {
+    "revision": "b2538c9ca6ca8bd4dae3",
+    "url": "/Pong-Pong/static/js/runtime-main.5b1ca55a.js"
+  },
+  {
+    "revision": "a608dd9c1a6d4fc9c7afe67505a546f2",
+    "url": "/Pong-Pong/static/media/Cheering.a608dd9c.mp3"
+  },
+  {
+    "revision": "6465cb7f8c649b68b2bb55138aade7ec",
+    "url": "/Pong-Pong/static/media/ball in play music.6465cb7f.mp3"
+  },
+  {
+    "revision": "05e8f5940a745ae07f9c75b05cfc0644",
+    "url": "/Pong-Pong/static/media/ballImpacts.05e8f594.mp3"
+  },
+  {
+    "revision": "b5b3aad95fe44cc50f5c5106e7209b51",
+    "url": "/Pong-Pong/static/media/ballSpeedsUp.b5b3aad9.wav"
+  },
+  {
+    "revision": "5b4e8c3281ab6a6b894198a0f6494d6a",
+    "url": "/Pong-Pong/static/media/final score music.5b4e8c32.wav"
+  },
+  {
+    "revision": "04de8cf87f4b4a56f08c0d5fe6b0d2f9",
+    "url": "/Pong-Pong/static/media/sports-whistle.04de8cf8.mp3"
+  },
+  {
+    "revision": "3b3ce360083ecd61a4b26c18cf7d4600",
+    "url": "/Pong-Pong/static/media/title screen music.3b3ce360.mp3"
+  },
+  {
+    "revision": "3853c0fff491883ecf0887634cbd2549",
+    "url": "/Pong-Pong/static/media/victory music2.3853c0ff.mp3"
+  }
+]);
